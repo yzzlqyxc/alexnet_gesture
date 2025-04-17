da2685d128e94514f1ee6cc6bc3b7d7f18f848c0
Xincheng Yu <xy2654@columbia.edu>
2025-04-17 00:26:11 -0400

    adds function to inference from picture

--- inf_dwarf.py
+++ inf_dwarf.py
@@ -7,51 +7,78 @@ from torchvision import models
 import torch.nn as nn
 import torchvision.transforms as transforms
 from train import Dwarf
+from pathlib import Path
 
-
-cap = cv2.VideoCapture(0)
-
-if not cap.isOpened():
-    print("无法打开摄像头")
-    exit()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Dwarf(num_classes=10).to(device)
-
 model.load_state_dict(torch.load("dwarf.pth", map_location=torch.device('cpu')))
-model.eval()
 model.to(device)
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5]*3, [0.5]*3)
+])
 
-while True:
-    # 逐帧捕获
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("无法接收帧，退出 ...")
-        break
+def inf_fromtenser(img):
+    model.eval()
 
-    resized = cv2.resize(frame, (32, 32))
-    cv2.imshow('Camera', resized)
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    transform = transforms.Compose([
-        transforms.Resize((32, 32)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.5]*3, [0.5]*3)
-    ])
-
-    img = Image.fromarray(rgb) 
+    img = Image.fromarray(img) 
     input_tensor = transform(img) 
     input_tensor = input_tensor.unsqueeze(0)  
 
     with torch.no_grad():
         outputs = model(input_tensor.to(device))
         predicted_class = outputs.argmax(dim=1).item()
-        print(predicted_class)
+        return predicted_class
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    time.sleep(0.1)
+def camera_inf():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("无法打开摄像头")
+        exit()
 
-# 释放资源
-cap.release()
-cv2.destroyAllWindows()
+    while True:
+        # 逐帧捕获
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("无法接收帧，退出 ...")
+            break
+
+        resized = cv2.resize(frame, (32, 32))
+        cv2.imshow('Camera', resized)
+        # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        print(inf_fromtenser(resized))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        time.sleep(0.1)
+
+    # 释放资源
+    cap.release()
+    cv2.destroyAllWindows()
+
+def pic_inf(pic_dir : str):
+    img = cv2.imread(pic_dir)
+    img = cv2.resize(img, (32, 32))
+
+    return inf_fromtenser(img)
+
+# pic_inf("./data/Dataset/0/IMG_1118.JPG")
+
+camera_inf()
+
+# for i in range(0, 10):
+#     a = './data/Dataset/' + str(i)
+#     path = Path(a)
+#     cnt, cntb, total = 0, 0, 0
+#     for file in path.rglob('*'):  # rglob() 用于递归查找所有文件和子文件夹
+#         a = pic_inf(file)
+#         # print(file, pic_inf(file))
+#         if a == i :
+#             cnt += 1
+#         elif a == 5:
+#             cntb += 1
+#         total += 1
+#     print(f"{i} accuray: {cnt/total}, {total} {cntb}")
